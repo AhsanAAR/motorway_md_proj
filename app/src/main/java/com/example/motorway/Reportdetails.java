e21be9ad05bfbd9cc36731abe9f703914aeb12ba
HamzaFareed2323 <hamzaq2323@gmail.com>
2022-02-09 02:26:52 +0500

    front end

--- app/src/main/java/com/example/motorway/Reportdetails.java
+++ app/src/main/java/com/example/motorway/Reportdetails.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ReportDetails extends AppCompatActivity {
+public class Reportdetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
